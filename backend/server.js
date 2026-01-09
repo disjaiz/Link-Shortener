c379e4867dea81b917ab128a2ef75a6760e6a4e8
Siddhi Jaiswal <siddhijaiz23@gmail.com>
2026-01-09 16:58:58 +0530

    updated cors origin fronend link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://192.168.1.6:5173'],
+  origin: 'https://link-shortener-frontend-i663.onrender.com',
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
